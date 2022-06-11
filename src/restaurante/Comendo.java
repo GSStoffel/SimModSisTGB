4b8f4836c5a3f770122f6b97264aefce54a4e78a
GSStoffel <gsperbstoffel@gmail.com>
2022-06-11 17:08:19 -0300

    Desenvolvimento do restaurante

---
+++ src/restaurante/Comendo.java
@@ -0,0 +1,10 @@
+package src.restaurante;
+
+import src.restaurante.Process;
+
+public class Comendo extends Process {
+
+    public Comendo(int processId, String name, double duration) {
+        super(processId, name, duration);
+    }
+}
