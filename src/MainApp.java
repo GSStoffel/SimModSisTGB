0a578c3243f7f20289ab0066863e8a443073a2f4
Carlos L.S. Machado <ucarlos555@gmail.com>
2022-06-12 13:54:43 -0300

    Ajustes

--- src/MainApp.java
+++ src/MainApp.java
@@ -7,14 +7,20 @@ public class MainApp {
     public static void main(String[] args) {
         Restaurante r = new Restaurante();
 
-        int bId = r.createResource("bancosBalcão", 1);
-        int m2Id = r.createResource("mesa2", 1);
-        int m4Id = r.createResource("mesa4", 1);
-        int atendenteCx1 = r.createResource("atendenteCx1", 1);
+        int bId = r.createResource("bancosBalcão", 6);
 
-        r.createResource("atendenteCx2", 1);
-        r.createResource("cozinheiros", 1);
-        r.createResource("", 1);
+        int mesa2Id = r.createResource("mesa2", 4);
+        int mesa4Id = r.createResource("mesa4", 4);
+
+        int atendenteCx1 = r.createResource("atendenteCx1", 1);
+        int atendenteCx2 = r.createResource("atendenteCx2", 1);
+
+        int cozinheirosId = r.createResource("cozinheiros", 3);
+
+
 
     }
 }
+//se tu ta zoiunho vai pro balcao
+//se 3 ou mais vai f4
+//se 2oumeno vai pra mesa que tem menos(2 ou 4)
